4b448c862f96f796206c990557a6a5f4e2c9ade2
ZaMiah <zamiahbell@gmail.com>
2020-11-20 23:07:50 -0500

    commit tree 07d214cdda1f3bf87c9d4b1c40f563c247ed9d0a

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(argument)
+  
+  
+  i = 0
+  while i < argument.size
+    block_given?
+    yield(argument[i])
+    i += 1
+  end
+  
+  argument   
 end
